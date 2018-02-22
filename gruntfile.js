05159ef6e0e32b4876fabb56bf9c0bc8ee85993a
geertsoetens <geert.soetens@ictmbo.nl>
2018-02-22 16:28:11 +0100

    Completed Webapp template. Gruntfile.js finished

--- gruntfile.js
+++ gruntfile.js
@@ -4,12 +4,47 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
-                banner: '/* banner comment */'
+                banner: '/* Javascript, uglified, makes it nice and hard to read doesn\'t it? */'
             },
             build: {
                 src: 'script.js',
                 dest: 'public/script.js'
             }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    src: ['*.css', '!*.css.min'],
+                    dest: 'public/',
+                    ext: '.css'
+                }]
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: {
+                    'public/script.js': 'script.js'
+                }
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                src: 'index.html',
+                dest: 'public/'
+            }
         }
-    })
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-babel');
+
+    grunt.registerTask('default', ['babel', 'uglify', 'cssmin', 'copy'])
 };
--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// DOMContent Loaded etc.
+window.addEventListener("DOMContent", function() {
+    console.log("The DOM has been loaded");
+    console.log("Not only that, but the script should be uglified now, attempt to read it...");
+
+});
